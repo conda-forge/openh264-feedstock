7cab86f0a68cc28c1a2bf29bbcbc019ae67f1344
Mark Harfouche <mark.harfouche@gmail.com>
2022-07-03 18:08:56 -0400

    Try

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,3 +1,3 @@
 copy /Y %RECIPE_DIR%\build_windows.sh build_windows.sh
-bash build_windows.sh
+call %BUILD_PREFIX%\Library\bin\run_autotools_clang_conda_build.bat build_windows.sh
 if %ERRORLEVEL% neq 0 exit 1
--- recipe/build_windows.sh
+++ recipe/build_windows.sh
@@ -1,18 +1,23 @@
 set -ex
 
+LDFLAGS="${LDFLAGS} -lucrt.lib"
+LDFLAGS="${LDFLAGS} -lvcruntime.lib"
+LDFLAGS="${LDFLAGS} -loldnames.lib"
+
 MAKE_FLAGS=
 MAKE_FLAGS="${MAKE_FLAGS} LIBSUFFIX=lib"
 MAKE_FLAGS="${MAKE_FLAGS} LIBPREFIX= "
 MAKE_FLAGS="${MAKE_FLAGS} OS=mingw_nt"
-MAKE_FLAGS="${MAKE_FLAGS} CC=x86_64-w64-mingw32-gcc"
-MAKE_FLAGS="${MAKE_FLAGS} CXX=x86_64-w64-mingw32-g++"
-MAKE_FLAGS="${MAKE_FLAGS} AR=x86_64-w64-mingw32-gcc-ar"
-MAKE_FLAGS="${MAKE_FLAGS} NM=x86_64-w64-mingw32-gcc-nm"
-MAKE_FLAGS="${MAKE_FLAGS} PREFIX=${CYGWIN_PREFIX}/Library"
+MAKE_FLAGS="${MAKE_FLAGS} CC=${CC}"
+MAKE_FLAGS="${MAKE_FLAGS} CXX=${CXX}"
+MAKE_FLAGS="${MAKE_FLAGS} AR=${AR}"
+MAKE_FLAGS="${MAKE_FLAGS} NM=${NM}"
+MAKE_FLAGS="${MAKE_FLAGS} PREFIX=${PREFIX}"
 MAKE_FLAGS="${MAKE_FLAGS} ASM=${BUILD_PREFIX}/Library/bin/nasm.exe"
 
 # make ${MAKE_FLAGS} clean
-make ${MAKE_FLAGS} -j${CPU_COUNT}
+make ${MAKE_FLAGS}
+# -j${CPU_COUNT}
 
 make ${MAKE_FLAGS} install
 rm ${CYGWIN_PREFIX}/Library/lib/openh264.dll.a
@@ -20,5 +25,3 @@ rm ${CYGWIN_PREFIX}/Library/lib/openh264.dll.a
 mkdir -p -m755 -v "$PREFIX"/bin
 install -m755 -v h264dec "${CYGWIN_PREFIX}/Library/bin/h264dec"
 install -m755 -v h264enc "${CYGWIN_PREFIX}/Library/bin/h264enc"
-# Manually replace cygwin_prefix with prefix in the pkgconfig file???
-sed -i "s,${CYGWIN_PREFIX},${PREFIX}," "${CYGWIN_PREFIX}/Library/lib/pkgconfig/openh264.pc
